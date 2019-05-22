1bbefbce198618ac599a7b1a25fc4549dec9821a
Piero Romero <elpierito3005@gmail.com>
2019-05-22 08:02:46 -0500

    E2: Definición del arreglo

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+#Designar el arreglo
+print("Definiendo el arreglo")
+n = int(input("Defina el número de términos del arreglo: "))
+D = []
+i = 0
+while i < n:
+    t = int(input("Ingrese un número: "))
+    D.append(t)
+    i = i + 1 
+
+print(D)
